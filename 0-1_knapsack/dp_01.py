5dfc533e3093dc133dea089ee0ac6d81a0a75327
Alice Chen <alice.chen@uconn.edu>
2025-04-24 15:52:49 -0400

    Update dp_01.py

--- 0-1_knapsack/dp_01.py
+++ 0-1_knapsack/dp_01.py
@@ -1,1 +1,41 @@
 # Alice Chen
+
+def knapsack(W, weight, profit):
+    """
+    W = maximum weight capacity 
+    weight = list of item weights
+    profit = list of item profits/values
+    """
+    n = len(weight) # number of items 
+    
+    # initializing 2D DP table (W+1 * n+1) with 0s
+    # stores the maximum value we can get using i items such that the knapsack capacity is j
+    dp = [[0 for _ in range(W+1)] for _ in range(n+1)]
+
+    # Build table dp[][] in bottom-up manner
+    for i in range(n+1):
+        for j in range(W+1):
+
+            # if there is no item or the knapsack's capacity is 0
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+            else:
+                pick = 0 
+
+                # pick ith item if it does not exceed the capacity of knapsack
+                if weight[i-1] <= j: 
+                    pick = profit[i - 1] + dp[i - 1][j - weight[i - 1]]
+
+                # don't pick
+                notPick = dp[i - 1][j]
+
+                dp[i][j] = max(pick, notPick)
+
+    return dp[n][W]
+
+    # O(n*W) time and space (tabulation)
+    # O(W) space for 1D dp table
+    # handles large n but not large W
+
+# add test cases
+
