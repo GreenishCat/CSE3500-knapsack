cc7a70e0ef8aa463d92a913cb364d136c6ad5b58
Alice Chen <alice.chen@uconn.edu>
2025-04-24 20:32:32 -0400

    Update dp_01.py

--- 0-1_knapsack/dp_01.py
+++ 0-1_knapsack/dp_01.py
@@ -9,25 +9,25 @@ def knapsack(W, weight, value):
     n = len(weight) # number of items 
     
     # initializing 2D DP table (W+1 * n+1) with 0s
-    # stores the maximum value we can get using i items such that the knapsack capacity is j
+    # stores the maximum value we can get using i items such that the knapsack capacity is w
     dp = [[0 for _ in range(W+1)] for _ in range(n+1)]
 
     # Build table dp[i][j] using bottom-up tabulation 
     for i in range(n+1):
-        for j in range(W+1):
+        for w in range(W+1):
 
             # if there is no item or the knapsack's capacity is 0
             if i == 0 or j == 0:
-                dp[i][j] = 0
+                dp[i][w] = 0
             else:
                 pick = 0 
 
                 # pick ith item if it does not exceed the capacity of knapsack
                 if weight[i-1] <= j: 
-                    pick = value[i - 1] + dp[i - 1][j - weight[i - 1]]
+                    pick = value[i - 1] + dp[i - 1][w - weight[i - 1]]
 
                 # don't pick
-                notPick = dp[i - 1][j]
+                notPick = dp[i - 1][w]
 
                 dp[i][j] = max(pick, notPick)
 
